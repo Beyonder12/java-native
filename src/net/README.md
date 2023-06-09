e3a61e3c9f70bfa2f994ba4bae76bacecaeb14e9
Fajri Illahi <fajri.illahi@ifg-life.id>
2023-06-09 22:41:28 +0700

    Postgresql driver without pom still faling

--- src/net/README.md
+++ src/net/README.md
@@ -1,1 +1,15 @@
 curl localhost:8000/hello
+(Invoke-WebRequest -Uri http://localhost:9000/hello)
+
+(Invoke-WebRequest -Uri http://localhost:9000/hello).Content | % {[char]$_}
+
+-join ((Invoke-WebRequest -Uri http://localhost:9000/hello).Content | % {[char]$_})
+
+
+Invoke-WebRequest -Uri https://jdbc.postgresql.org/download/postgresql-42.2.20.jar -OutFile postgresql-42.2.20.jar
+
+cd "C:\Users\IFG Life\Documents\Github\fajri\java-native\src"
+
+javac -cp .`;`postgresql-42.2.20.jar SimpleHttpServer.java
+
+java -cp .`;`postgresql-42.2.20.jar`;`src net.SimpleHttpServer
--- src/net/SimpleHttpServer.java
+++ src/net/SimpleHttpServer.java
@@ -1,23 +1,50 @@
 package net;
 
 import com.sun.net.httpserver.*;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.sql.*;
 
 public class SimpleHttpServer {
 
     public static void main(String[] args) throws Exception {
+        System.out.println("Listening...");
         HttpServer server = HttpServer.create(new InetSocketAddress(9000), 0);
-        HttpContext context = server.createContext("/hello");
-        context.setHandler(SimpleHttpServer::handleRequest);
+        server.createContext("/hello", SimpleHttpServer::handleHelloRequest);
+        server.createContext("/people", SimpleHttpServer::handlePeopleRequest);
         server.start();
     }
 
-    private static void handleRequest(HttpExchange exchange) throws IOException {
+    private static void handleHelloRequest(HttpExchange exchange) throws IOException {
         String response = "Hello, world!";
-        exchange.sendResponseHeaders(200, response.getBytes().length); // 200 is http status code (OK)
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+
+    private static void handlePeopleRequest(HttpExchange exchange) throws IOException {
+        String response;
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5435/localdb",
+                    "fajri",
+                    "12345678"
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            while (resultSet.next()) {
+                stringBuilder.append(resultSet.getString("name")).append("\n");
+            }
+            response = stringBuilder.toString();
+        } catch (Exception e) {
+            response = "Error: " + e.getMessage();
+        }
+        exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
